0749f8561c08f46075d6fb4548ddd35329637f90
Colin-Cai-Uni <Colin@vlan-2645-10-16-186-31.student.wireless.sydney.edu.au>
2017-04-24 13:31:19 +1000

    Revert "Merge pull request #3 from Colin-Cai-Uni/Diffie-Hellman"
    This reverts commit 3940275ea077d5aa3ec0954adf754a6472558181, reversing
    changes made to ae9046bc1c9022ef25a63a7e2167b9cff9d30507.

--- dh/__init__.py
+++ dh/__init__.py
@@ -5,7 +5,7 @@ from lib.helpers import read_hex
 
 # Project TODO: Is this the best choice of prime? Why? Why not? Feel free to replace!
 
-# 3072 bit safe prime for Diffie-Hellman key exchange
+# 1536 bit safe prime for Diffie-Hellman key exchange
 # obtained from RFC 3526
 raw_prime = """FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
 29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
@@ -14,15 +14,7 @@ E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
 EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
 C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
 83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
-670C354E 4ABC9804 F1746C08 CA18217C 32905E46 2E36CE3B
-E39E772C 180E8603 9B2783A2 EC07A28F B5C55DF0 6F4C52C9
-DE2BCBF6 95581718 3995497C EA956AE5 15D22618 98FA0510
-15728E5A 8AAAC42D AD33170D 04507A33 A85521AB DF1CBA64
-ECFB8504 58DBEF0A 8AEA7157 5D060C7D B3970F85 A6E1E4C7
-ABF5AE8C DB0933D7 1E8C94E0 4A25619D CEE3D226 1AD2EE6B
-F12FFA06 D98A0864 D8760273 3EC86A64 521F2B18 177B200C
-BBE11757 7A615D6C 770988C0 BAD946E2 08E24FA0 74E5AB31
-43DB5BFC E0FD108E 4B82D120 A93AD2CA FFFFFFFF FFFFFFFF"""
+670C354E 4ABC9804 F1746C08 CA237327 FFFFFFFF FFFFFFFF"""
 # Convert from the value supplied in the RFC to an integer
 prime = read_hex(raw_prime)
 
@@ -31,14 +23,14 @@ prime = read_hex(raw_prime)
 def create_dh_key():
     # Creates a Diffie-Hellman key
     # Returns (public, private)
-    a = random.randint(2, prime - 2)
+    a = random.randint(0, int(2**8))
     g = 2 #obtained from MODP RFC 3526
     Public_key = pow(g,a,prime)
-    return (Public_key, a)
+    return (a, Public_key)
 
 def calculate_dh_secret(their_public, my_private):
     # Calculate the shared 
-    shared_secret = pow(their_public,my_private,prime)
+    shared_secret = pow(their_public,my_private,prime)cret
     
     # Hash the value so that:
     # (a) There's no bias in the bits of the output
@@ -46,5 +38,5 @@ def calculate_dh_secret(their_public, my_private):
     # (b) We can convert to raw bytes easily
     # (c) We could add additional information if we wanted
     # Feel free to change SHA256 to a different value if more appropriate
-    shared_hash = SHA256.new(bytes(str(shared_secret), "ascii")).hexdigest()
+    shared_hash = SHA256.new(bytes(shared_secret, "ascii")).hexdigest()
     return shared_hash
